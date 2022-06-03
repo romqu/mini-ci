c86ec604d86a9b459d3f24aa127d90fc2cd43378
Roman Q <roman.quistler@gmail.com>
2022-06-03 19:08:56 +0200

    wip

--- src/data/github_webhook_repository.rs
+++ src/data/github_webhook_repository.rs
@@ -1,7 +1,11 @@
 use std::sync::{Arc, Mutex};
 
+use actix_service::Service;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use serde::de::DeserializeOwned;
+
+use crate::data::github_webhook_repository::ApiCallError::{DtoToJsonStringError, JsonToDtoError, SendError};
 
 pub struct GithubWebhookRepository {
     api_client: Arc<Mutex<Client>>,
@@ -17,26 +21,47 @@ impl GithubWebhookRepository {
         owner: &'static str,
         repo: &'static str,
         dto: GithubWebhookCreateDto,
-    ) -> Result<GithubWebhookDto, reqwest::Error> {
+    ) -> Result<Box<GithubWebhookCreateDto>, ApiCallError> {
         let url = format!(
             "https://api.github.com/repos/{owner}/{repo}/hooks",
             owner = owner,
             repo = repo
         );
 
-        let response = self
-            .api_client
+        self.execute_post_call(url, &dto).await
+    }
+
+    // static string causes hidden lifetime
+    async fn execute_post_call<T>(&self, url: String, dto: &T) -> Result<Box<T>, ApiCallError>
+        where
+            T: ?Sized + Serialize + DeserializeOwned,
+    {
+        let body = serde_json::to_string(&dto).map_err(|_| DtoToJsonStringError)?;
+
+        self.api_client
             .lock()
             .unwrap()
             .post(url)
-            .body(serde_json::to_string(&dto).unwrap())
+            .body(body)
             .send()
-            .await?;
-
-        let github_webhook_dto = response.json::<GithubWebhookDto>().await?;
-
-        Ok(github_webhook_dto)
+            .await
+            .map_err(|err| {
+                println!("{}", err);
+                SendError
+            })?
+            .json::<Box<T>>()
+            .await
+            .map_err(|err| {
+                println!("{}", err);
+                JsonToDtoError
+            })
     }
+}
+
+pub enum ApiCallError {
+    DtoToJsonStringError,
+    SendError,
+    JsonToDtoError,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
